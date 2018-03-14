a092878d39180df78b42491d86179b6dba64729e
jekoff <_maks70@list.ru>
2018-03-14 14:04:22 +0200

    base code structure added

--- main.py
+++ main.py
@@ -3,5 +3,31 @@ import config
 
 bot = telebot.TeleBot(config.token)
 
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    pass
+
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    pass
+
+
+@bot.message_handler(commands=['anekdot'])
+def send_joke(message):
+    pass
+
+
+@bot.message_handler(commands=['category'])
+def send_categories(message):
+    pass
+
+
+@bot.message_handler(content_type=['text'])
+def send_answer_for_text(message):
+    pass
+
+
 if __name__ == '__main__':
     bot.polling(none_stop=True)
