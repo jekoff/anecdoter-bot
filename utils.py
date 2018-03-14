b3995c0e69efdf8262f8a6bb7f118dbe1c110da9
jekoff <_maks70@list.ru>
2018-03-14 17:03:42 +0200

    help utils added

---
+++ utils.py
@@ -0,0 +1,14 @@
+import emoji
+
+
+def emojize(string):
+    return emoji.emojize(string, use_aliases=True)
+
+
+def unescape(s):
+    s = s.replace("&lt;", "<")
+    s = s.replace("&gt;", ">")
+    s = s.replace("&quot;", '"')
+    s = s.replace("&apos;", "'")
+    s = s.replace("&amp;", "&")
+    return s
