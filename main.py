c91ead0e05799f86c87dbf7980e20c1a5d9d2d8c
jekoff <_maks70@list.ru>
2018-03-14 17:51:08 +0200

    "more" button added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import telebot
 import config
 from sqlighter import SQLighter
 from utils import emojize,unescape
+from telebot import types
 
 bot = telebot.TeleBot(config.TOKEN)
 
@@ -12,7 +13,7 @@ def send_welcome(message):
     reply_text = "Привет, меня зовут Бот Анекдот!:sunglasses:\n " \
                  "Я знаю очень много смешных анекдотов. :satisfied:\n" \
                  "Напиши мне /anekdot, и расскажу тебе один из них"
-    bot.reply_to(message, emojize(reply_text))
+    bot.send_message(message.chat.id, emojize(reply_text))
 
 
 @bot.message_handler(commands=['help'])
@@ -20,11 +21,20 @@ def send_help(message):
     pass
 
 
+@bot.callback_query_handler(lambda t: True)
+def call_inline(call):
+    if call.message:
+        if call.data=='anekdot':
+            send_joke(call.message)
+
+
 @bot.message_handler(commands=['anekdot'])
 def send_joke(message):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(types.InlineKeyboardButton(text="Еще анекдот!", callback_data="anekdot"))
     sqlighter = SQLighter(config.DB_PATH)
     reply_text = "{0} :satisfied:".format(sqlighter.get_random_joke())
-    bot.send_message(message.chat.id, unescape(emojize(reply_text)))
+    bot.send_message(message.chat.id, unescape(emojize(reply_text)),reply_markup=keyboard)
 
 
 @bot.message_handler(commands=['category'])
