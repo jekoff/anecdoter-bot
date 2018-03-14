9e47e41f63722a3bcfb3f231164acb9b39e21bce
jekoff <_maks70@list.ru>
2018-03-14 17:02:58 +0200

    func for random joke added

--- sqlighter.py
+++ sqlighter.py
@@ -1,4 +1,6 @@
 import sqlite3
+import os
+import config
 
 
 class SQLighter:
@@ -7,8 +9,13 @@ class SQLighter:
         self.connection = sqlite3.connect(database)
         self.cursor = self.connection.cursor()
 
-
+    def get_random_joke(self):
+        query = "SELECT * FROM aneks LIMIT 1 OFFSET ABS(RANDOM()) % MAX((SELECT COUNT(*) FROM aneks), 1)"
+        with self.connection:
+            return self.cursor.execute(query).fetchone()[2]
 
     def close(self):
         """ Закрываем текущее соединение с БД """
         self.connection.close()
+
+
